38c51c4ece11f3880504ba22530a6dfd836d55bc
Anarbek4646 <londonisthecapita66@gmail.com>
2024-03-25 16:41:39 +0600

    rizz33

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,6 @@
-from django.db import models
+# from django.db import models
 
-# Create your models here.
+
 from django.conf import settings
 from django.db import models
 from django.utils import timezone
@@ -19,3 +19,65 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+#
+#
+#
+#
+# class Person(models.Model):
+#     name = models.CharField(max_length=255)
+#     age = models.IntegerField()
+#
+#     class Meta:
+#         verbose_name_plural = 'People'
+#         verbose_name = 'Person'
+#
+#     def __str__(self):
+#         return self.name
+#
+# class Cat(models.Model):
+#     color = models.CharField(max_length=255)
+#     name = models.CharField(max_length=255)
+#     mood = models.CharField(max_length=255)
+#     owner = models.ForeignKey(Person,on_delete=models.CASCADE,related_name="people", null=True, blank=True)
+#     age = models.IntegerField()
+#
+#     class Meta:
+#         verbose_name_plural = 'Cats'
+#         verbose_name = 'Cat'
+#
+#     def __str__(self):
+#         return self.name
+#
+#     def purr(self):
+#         return f'{self.name} мурчит'
+#
+#     def scratch(self):
+#         return f'{self.name} царапаеться'
+#
+#     def feed(self):
+#         return f'{self.name} покормили {CatFood.food}со вкусом {CatFood.taste}'
+#
+# class CatFood(models.Model):
+#     food = models.CharField(max_length=255)
+#     taste = models.CharField(max_length=255)
+#
+#     class Meta:
+#         verbose_name_plural = 'Кошачья Еда'
+#         verbose_name = 'Еда'
+#
+#     def __str__(self):
+#         return self.food
+#
+# class Post(models.Model):
+#     title = models.CharField(max_length=255)
+#     text = models.TextField(max_length=255)
+#     author = models.ForeignKey(Person,related_name="people", blank=True, null=True)
+#     created_date = models.DateTimeField(auto_now_add=True, blank=True)
+#     published_date = models.DateTimeField(auto_now=True, blank=True)
+#
+#     class Meta:
+#         verbose_name_plural = 'Posts'
+#         verbose_name = 'Post'
+#
+#     def __str__(self):
+#         return self.title
